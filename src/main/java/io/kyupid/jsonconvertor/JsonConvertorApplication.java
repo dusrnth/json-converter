2d20b258958382676e78e9176f10bea06964d111
Yeonwoo <kyu@whatap.io>
2024-03-25 23:51:18 +0900

    초기화

---
+++ src/main/java/io/kyupid/jsonconvertor/JsonConvertorApplication.java
@@ -0,0 +1,13 @@
+package io.kyupid.jsonconvertor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonConvertorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonConvertorApplication.class, args);
+	}
+
+}
