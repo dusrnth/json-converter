c753c5039f546822c8d8699aa512143d2f3dfd88
kyupid <iamscvv@gmail.com>
2024-03-26 22:04:06 +0900

    #2 add: JSONParser 메소드 목록 추가

--- src/main/java/io/kyupid/jsonconvertor/json/JSONParser.java
+++ src/main/java/io/kyupid/jsonconvertor/json/JSONParser.java
@@ -1,4 +1,59 @@
 package io.kyupid.jsonconvertor.json;
 
 public class JSONParser {
+    public Object parse(String json) {
+        
+    }
+
+    private Token nextToken() {
+        
+    }
+
+    private TokenType getTokenType() {
+        
+    }
+
+    private Map<String, Object> parseObject() {
+        
+    }
+
+    private void parseObjectProperty() {
+        
+    }
+
+    private List<Object> parseArray() {
+        
+    }
+
+    private Object parseValue() {
+        
+    }
+
+    private String parseString() {
+        
+    }
+
+    private Number parseNumber() {
+        
+    }
+
+    private Boolean parseBoolean() {
+        
+    }
+
+    private Object parseNull() {
+        
+    }
+
+    private void skipWhitespace() {
+        
+    }
+
+    private void expect(char expected) {
+        
+    }
+
+    private boolean isEndOfString() {
+        
+    }
 }
