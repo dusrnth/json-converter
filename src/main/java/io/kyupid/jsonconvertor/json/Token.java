9d279f89cecae3d3b5b6733ab26c50b81775b932
kyupid <iamscvv@gmail.com>
2024-03-26 23:41:25 +0900

    #3 refactor: 고정된 것과 변동적인 TokenType 으로 구분이 됨에 따라 리팩토링
    - FIXED_TOKENS 을 통해 고정된 토큰은 재사용하도록 설계

--- src/main/java/io/kyupid/jsonconvertor/json/Token.java
+++ src/main/java/io/kyupid/jsonconvertor/json/Token.java
@@ -1,35 +1,63 @@
 package io.kyupid.jsonconvertor.json;
 
-public enum Token {
-    LEFT_BRACE("{"),
-    RIGHT_BRACE("}"),
-    DOUBLE_QUOTES("\""),
-    LEFT_BRACKET("["),
-    RIGHT_BRACKET("]"),
-    COLON(":"),
-    COMMA(","),
-    TRUE("true"),
-    FALSE("false"),
-    NULL("null"),
-    STRING(null),
-    NUMBER(null),
-    END_OF_STRING(null);
+import java.util.*;
 
-    private String value;
+public final class Token {
 
-    Token(String value) {
+    private static final Map<TokenType, Token> FIXED_TOKENS = new EnumMap<>(TokenType.class);
+
+    static {
+        for (TokenType type : TokenType.values()) {
+            if (type != TokenType.STRING && type != TokenType.NUMBER) {
+                FIXED_TOKENS.put(type, new Token(type, null));
+            }
+        }
+    }
+
+    private final TokenType type;
+    private final String value;
+
+    private Token(TokenType type, String value) {
+        this.type = type;
         this.value = value;
+    }
+
+    public static Token of(TokenType type, String value) {
+        if (type == TokenType.STRING || type == TokenType.NUMBER) {
+            return new Token(type, value);
+        }
+        throw new IllegalArgumentException("Only STRING and NUMBER tokens can have a value");
+    }
+
+    public static Token of(TokenType type) {
+        Token token = FIXED_TOKENS.get(type);
+        if (token != null) {
+            return token;
+        }
+        // todo string, number 의 처리 ?
+        throw new IllegalArgumentException("Invalid token type: " + type);
+    }
+
+    public TokenType getType() {
+        return type;
     }
 
     public String getValue() {
         return value;
     }
 
-    public void setValue(String value) {
-        if (this == STRING || this == NUMBER || this == END_OF_STRING) {
-            this.value = value;
-        } else {
-            throw new IllegalStateException("Cannot set value for token: " + this);
-        }
+    public enum TokenType {
+        LEFT_BRACE,
+        RIGHT_BRACE,
+        DOUBLE_QUOTES,
+        LEFT_BRACKET,
+        RIGHT_BRACKET,
+        COLON,
+        COMMA,
+        TRUE,
+        FALSE,
+        NULL,
+        STRING,
+        NUMBER
     }
 }
