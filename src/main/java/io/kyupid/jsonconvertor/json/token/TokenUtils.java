241d8f80b3cf2b4a02a7a884ff1631bb4bf73264
Yeonwoo <kyu@whatap.io>
2024-03-29 22:06:28 +0900

    #8 기본 타입 토에 대한 캐시 처리

--- src/main/java/io/kyupid/jsonconvertor/json/token/TokenUtils.java
+++ src/main/java/io/kyupid/jsonconvertor/json/token/TokenUtils.java
@@ -5,11 +5,20 @@ import java.util.Map;
 
 public final class TokenUtils {
     private static final Map<Character, TokenType> CHAR_TO_TOKEN_TYPE = new HashMap<>();
+    private static final Map<TokenType, Token> CACHED_TOKENS = new HashMap<>();
 
     static {
         for (TokenType type : TokenType.values()) {
             CHAR_TO_TOKEN_TYPE.put(type.getChar(), type);
         }
+
+        // 기본 타입 토큰만 캐시 처리
+        CACHED_TOKENS.put(TokenType.LEFT_BRACE, new Token(TokenType.LEFT_BRACE, null));
+        CACHED_TOKENS.put(TokenType.RIGHT_BRACE, new Token(TokenType.RIGHT_BRACE, null));
+        CACHED_TOKENS.put(TokenType.LEFT_BRACKET, new Token(TokenType.LEFT_BRACKET, null));
+        CACHED_TOKENS.put(TokenType.RIGHT_BRACKET, new Token(TokenType.RIGHT_BRACKET, null));
+        CACHED_TOKENS.put(TokenType.COLON, new Token(TokenType.COLON, null));
+        CACHED_TOKENS.put(TokenType.COMMA, new Token(TokenType.COMMA, null));
     }
 
     private TokenUtils() {}
@@ -19,10 +28,9 @@ public final class TokenUtils {
     }
 
     public static Token createToken(TokenType type, String value) {
-        if (type == TokenType.STRING || type == TokenType.NUMBER) {
-            return new Token(type, value);
+        if (CACHED_TOKENS.containsKey(type)) {
+            return CACHED_TOKENS.get(type);
         }
-        // todo 나머지 타입 토큰들은 막을 것인가?
-        return new Token(type, null);
+        return new Token(type, value);
     }
 }
